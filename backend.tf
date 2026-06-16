daa6a666b8c0b042b3d248edae90a43cce4f799a
jangirrahul <jangirurafuru@gmail.com>
2026-06-16 23:54:37 +0900

    fix

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "my-bucket-908835050269-ap-northeast-3-an"
-    key          = "terraform.tfstate"
-    region       = "ap-northeast-3"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
